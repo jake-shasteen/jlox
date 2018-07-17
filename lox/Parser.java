0cde2bfd7d6ff1f0ff421a911b6cc54c39120dd4
Jake Shasteen <jakeshasteen@gmail.com>
2018-07-17 01:03:56 -0700

    Add synchronize() and parse()

--- lox/Parser.java
+++ lox/Parser.java
@@ -21,6 +21,14 @@ class Parser {
     this.tokens = tokens;
   }
 
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
   private Expr expression() {
     return equality();
   }
@@ -139,6 +147,28 @@ class Parser {
   private ParseError error(Token token, String message) {
     Lox.error(token, message);
     return new ParseError();
+  }
+
+  private void synchronize() {
+    advance();
+
+    while(!isAtEnd()) {
+      if(previous().type == SEMICOLON) return;
+
+      switch(peek().type) {
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+      }
+
+      advance();
+    }
   }
 
   private boolean isAtEnd() {
